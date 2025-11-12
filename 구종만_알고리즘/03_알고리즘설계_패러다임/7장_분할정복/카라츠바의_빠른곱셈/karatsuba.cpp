0012ccdcfdb7386e2d448c4dd7460064a3373f71
KWON, HAECHAN <146458940+haechancoco@users.noreply.github.com>
2025-11-12 14:26:02 +0000

    karatsuba

--- 구종만_알고리즘/03_알고리즘설계_패러다임/7장_분할정복/카라츠바의_빠른곱셈/karatsuba.cpp
+++ 구종만_알고리즘/03_알고리즘설계_패러다임/7장_분할정복/카라츠바의_빠른곱셈/karatsuba.cpp
@@ -41,8 +41,11 @@
 */
 
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+// 일반적인 큰 수 곱셈
+vector<int> multiple(const vector<int> a, const vector<int> b);
 
 // a += b * (10^k); 를 구현한다
 void addTo(vector<int>& a, const vector<int>& b, int k);
@@ -59,5 +62,35 @@ vector<int> karatsuba(const vector<int>& a, const vector<int>& b) {
 
   // 기저 사례: a 나 b 가 비어 있는 경우
   if (an == 0 || bn == 0) return vector<int>();
-}
 
+  // 기저 사례: a가 비교적 짧을 경우 O(n^2) 곱셈으로 변경한다
+  if (an <= 50) return multiple(a, b);
+
+  int half = an / 2;
+  // a 와 b 를 밑에서 half 자리와 나머지로 분리한다
+  vector<int> a0(a.begin(), a.begin() + half);
+  vector<int> a1(a.begin() + half, a.end());
+  vector<int> b0(b.begin(), b.begin() + min<int>(b.size(), half));
+  vector<int> b1(b.begin() + min<int>(b.size(), half), b.end());
+
+  // z2 = a1 * b1
+  vector<int> z2 = karatsuba(a1, b1);
+
+  // z0 = a0 * b0
+  vector<int> z0 = karatsuba(a0, b0);
+
+  // a0 = a0 + a1; b0 = b0 + b1;
+  addTo(a0, a1, 0); addTo(b0, b1, 0);
+
+  // z1 = (a0*b0) - z0 - z2
+  vector<int> z1 = karatsuba(a0, b0);
+  subFrom(z1, z0);
+  subFrom(z1, z2);
+
+  // ret = z0 + z1*10^half + z2*10^(half*2)
+  vector<int> ret;
+  addTo(ret, z0, 0);
+  addTo(ret, z1, half);
+  addTo(ret, z2, half * 2);
+  return ret;
+}
